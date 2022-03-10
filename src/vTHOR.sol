34ef3634f77de26bd3099718f1f5a09d970077bc
senkun33t <senkun33t@protonmail.com>
2022-03-10 13:56:11 -0500

    fix audit PVE005

--- src/vTHOR.sol
+++ src/vTHOR.sol
@@ -4,8 +4,9 @@ pragma solidity 0.8.10;
 import { ERC20Vote } from "../lib/ERC20Vote.sol";
 import { IERC20 } from "./interfaces/IERC20.sol";
 import { SafeTransferLib } from "../lib/SafeTransferLib.sol";
+import { ReentrancyGuard } from "../lib/ReentrancyGuard.sol";
 
-contract vTHOR is ERC20Vote {
+contract vTHOR is ERC20Vote, ReentrancyGuard {
     using SafeTransferLib for address;
 
     address public token;
@@ -14,18 +15,18 @@ contract vTHOR is ERC20Vote {
         token = _token;
     }
 
-    function deposit(uint256 amount) public {
+    function deposit(uint256 amount) public nonReentrant {
         uint256 totalShares = totalSupply;
         uint256 totalDeposits = IERC20(token).balanceOf(address(this));
-        token.safeTransferFrom(msg.sender, address(this), amount);
         if (totalShares == 0 || totalDeposits == 0) {
             _mint(msg.sender, amount);
         } else {
             _mint(msg.sender, (amount * totalShares) / totalDeposits);
         }
+        token.safeTransferFrom(msg.sender, address(this), amount);
     }
 
-    function withdraw(uint256 share) public {
+    function withdraw(uint256 share) public nonReentrant {
         uint256 totalShares = totalSupply;
         uint256 totalDeposits = IERC20(token).balanceOf(address(this));
         uint256 amount = (share * totalDeposits) / totalShares;
